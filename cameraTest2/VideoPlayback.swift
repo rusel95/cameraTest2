43cfa0b37dc0463b7c0641523aef958059b0f10e
rusel95 <ruslanpopesku95@gmail.com>
2017-07-11 11:27:18 +0300

    everything is fucking working

---
+++ cameraTest2/VideoPlayback.swift
@@ -0,0 +1,41 @@
+//
+//  VideoPlayback.swift
+//  cameraTest2
+//
+//  Created by Admin on 11.07.17.
+//  Copyright © 2017 rusel95. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class VideoPlayback: UIViewController {
+    
+    let avPlayer = AVPlayer()
+    var avPlayerLayer: AVPlayerLayer!
+    
+    var videoURL: URL!
+    //connect this to your uiview in storyboard
+    @IBOutlet weak var videoView: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        avPlayerLayer = AVPlayerLayer(player: avPlayer)
+        avPlayerLayer.frame = view.bounds
+        avPlayerLayer.videoGravity = AVLayerVideoGravityResizeAspectFill
+        videoView.layer.insertSublayer(avPlayerLayer, at: 0)
+        
+        view.layoutIfNeeded()
+        
+        let playerItem = AVPlayerItem(url: videoURL as URL)
+        avPlayer.replaceCurrentItem(with: playerItem)
+        
+        
+        avPlayer.play()
+        
+    }
+    
+    
+    
+}
